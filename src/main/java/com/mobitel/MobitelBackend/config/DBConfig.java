eb868f56a0bb3c7ff26aa7cff60c40e43aa2e524
00109 <00109@MUM-BOR-MR51-10.NIITEDUCATION.COM>
2017-07-11 09:30:55 +0530

    First Commit

---
+++ src/main/java/com/mobitel/MobitelBackend/config/DBConfig.java
@@ -0,0 +1,104 @@
+package com.mobitel.MobitelBackend.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mobitel.MobitelBackend.dao.*;       //added for all
+import com.mobitel.MobitelBackend.model.*;        //added for all
+
+@Configuration
+@ComponentScan("com.mobitel.MobitelBackend")
+@EnableTransactionManagement
+public class DBConfig {
+	
+	@Bean(name = "dataSource")
+	public DataSource getH2DataSource() 
+	{
+		System.out.println("Starting of the method getH2DataSource");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("");
+		dataSource.setUsername("lynal");
+		dataSource.setPassword("lynal");
+		System.out.println("Data Source Creation");
+		return dataSource;
+	}
+	
+	@Autowired
+	@Bean(name="sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource)
+	{
+		System.out.println("---Hibernate Properties----");
+		Properties prop=new Properties();
+		prop.setProperty("hibernate.hbm2ddl.auto", "update");
+		prop.put("hibernate.show_sql", "true"); //optional
+		prop.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		System.out.println("---Hibernate Properties Created");
+		
+		System.out.println("---Local SessionFactory Builder Object Creation---");
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(getH2DataSource());
+		sessionBuilder.addProperties(prop);
+		System.out.println("---Factory Builder Object Created---");
+		sessionBuilder.addAnnotatedClass(Category.class);
+		sessionBuilder.addAnnotatedClass(Supplier.class); // new added for supplier
+		sessionBuilder.addAnnotatedClass(Product.class);  // new added for product
+		sessionBuilder.addAnnotatedClass(User.class);  // new added for user
+		System.out.println("Session Factory Object Creation");
+		SessionFactory sessionFactory=sessionBuilder.buildSessionFactory();
+		System.out.println("Session Factory Object Created");
+		return sessionFactory;
+	}
+	
+	@Autowired
+	@Bean(name="transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
+	{
+		System.out.println("--Transaction manager Object Creation--");
+		HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory);
+		System.out.println("--Transaction manager Object Created--");
+		return transactionManager;
+	}
+	
+	@Autowired
+	@Bean(name="categoryDAO")
+	public CategoryDAO getCategoryDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("-- CategoryDAO Object Creation--");
+		return new CategoryDAO(sessionFactory);
+	}
+	
+	@Autowired
+	@Bean(name="supplierDAO")
+	public SupplierDAO getSupplierDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("-- SupplierDAO Object Creation--");
+		return new SupplierDAO(sessionFactory);
+	}
+	
+	@Autowired
+	@Bean(name="productDAO")
+	public ProductDAO getProductDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("-- ProductDAO Object Creation--");
+		return new ProductDAO(sessionFactory);
+	}
+	
+	@Autowired
+	@Bean(name="userDAO")
+	public UserDAO getUserDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("-- UserDAO Object Creation--");
+		return new UserDAO(sessionFactory);
+	}
+}
